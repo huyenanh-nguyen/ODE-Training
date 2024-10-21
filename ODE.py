11c6a931f40de0416f39903e2c13e04ed580bd91
huyenanh-nguyen <ng_huyenanh@mailbox.org>
2024-10-21 06:15:19 +0200

    examp

--- ODE.py
+++ ODE.py
@@ -16,7 +16,7 @@ def goodwill(par, t , v, k, n):
     v1, v2, v3, v4, v5, v6 = v
     k1,k2,k4,k6 = k
 
-    dx = (v1 * (k1**n / (k1**n + z**n))) - v2 * (x / (k2 + x))
+    dx = (v1 * (k1**n / (k1**n + z**n))) - (v2 * (x / (k2 + x)))
     dy = (v3 * x) - (v4 * ( y / (k4 + y)))
     dz = (v5 * y) - (v6 * (z / (k6 + z)))
 
@@ -42,6 +42,7 @@ class Goodwill_models:
         self.n = n
         self.t = t
         
+
     def goodwill_solver(self):
 
         t = self.t
@@ -50,12 +51,29 @@ class Goodwill_models:
         k = self.k
         n = self.n
 
-        return odeint(goodwill, par, t, args = (v, k , n))
+        return odeint(goodwill, par, t, args = (v, k, n))
+
+
+    def goodplot_timeseries(self):
+
+        solv = self.goodwill_solver()
+        plt.plot(t,solv[:,0],'g',label=r'$\frac{dx}{dt}= v_1 \frac{K_1^2}{K_1^n + z^n} - v_2 \frac{x}{K_2 + x}$')
+        plt.plot(t,solv[:,1],'r',label=r'$\frac{dy}{dt}= v_3x - v_4 \frac{y}{K_4 + y}$')
+        plt.plot(t,solv[:,2],'b',label=r'$\frac{dz}{dt}= v_5y - v_6 \frac{z}{K_6 + z}$')
+        plt.ylabel('concentraition [a.u.]')
+        plt.ylim(-1,5)
+        plt.xlim(200,300)
+        plt.xlabel('time [h]')
+        plt.legend(loc='best')
+        plt.show()
+
+        return None
+
+        
 
 
 
-
-
+# [examples]_______________________________________________________________________________________________________________________
 
 par = [0,0,0]
 v = [0.7, 0.45, 0.7, 0.35, 0.7, 0.35]
@@ -65,15 +83,7 @@ t = np.arange(0.0,1000, 0.01)
 
 good = Goodwill_models(par, v, k, n, t)
 
-z = good.goodwill_solver()
-print(z)
+solv = good.goodplot_timeseries()
+print(solv)
 
-plt.plot(t,z[:,0],'g',label=r'$\frac{dx}{dt}= v_1 \frac{K_1^2}{K_1^n + z^n} - v_2 \frac{x}{K_2 + x}$')
-plt.plot(t,z[:,1],'r',label=r'$\frac{dy}{dt}= v_3x - v_4 \frac{y}{K_4 + y}$')
-plt.plot(t,z[:,2],'b',label=r'$\frac{dz}{dt}= v_5y - v_6 \frac{z}{K_6 + z}$')
-plt.ylabel('concentraition [a.u.]')
-plt.ylim(-1,5)
-plt.xlim(200,300)
-plt.xlabel('time [h]')
-plt.legend(loc='best')
-plt.show()
+
